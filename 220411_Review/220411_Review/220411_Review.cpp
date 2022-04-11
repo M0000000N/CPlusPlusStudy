e46a2ca2a4fc72d21d34854e3f1cc6c3722569dc
M0000000N <tt6tt08233@gmail.com>
2022-04-11 15:52:59 +0900

    add project

---
+++ 220411_Review/220411_Review/220411_Review.cpp
@@ -0,0 +1,31 @@
+﻿// 220411_Review.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//int main()
+//{
+//    int input_key;
+//    std::cout << "helloworld!\n";
+//    std::cin >> input_key;
+//    float test = 5.3;
+//
+//}
+
+int main()
+{
+    // 보석토큰
+    string emerald = "e";
+    string diamond = "d";
+    string sapaire = "s";
+    string julmano = "j";
+    string ruby = "r";
+    string gold = "g";
+
+    
+        string myOwnTocken = emerald + emerald + diamond;
+        cout << myOwnTocken << endl;
+    
+}
